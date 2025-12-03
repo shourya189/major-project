23c64bf154b4159c65fdfbf499097698d5a02e2d
shourya189 <Shourya.27524@stu.upes.ac.in>
2025-12-03 22:44:33 +0530

    Create employee_operations.h

---
+++ include/employee_operations.h
@@ -0,0 +1,12 @@
+#ifndef EMPLOYEE_OPERATIONS_H
+#define EMPLOYEE_OPERATIONS_H
+
+#include "employee.h"
+
+void addEmployee();
+void viewEmployees();
+void searchEmployee();
+void updateEmployee();
+void deleteEmployee();
+
+#endif
